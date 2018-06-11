c4366f4ffc56ac96bcde80393360a6de552d087f
Tom <tom.clement@bizboard.nl>
2018-06-11 14:31:46 +0200

    Added preliminary gerbers

---
+++ hardware/gerbers/rename.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+mv cz18_badge-B.Cu.gbr cz18_badge.GBL
+mv cz18_badge-B.Mask.gbr cz18_badge.GBS
+mv cz18_badge-B.SilkS.gbr cz18_badge.GBO
+mv cz18_badge-F.Cu.gbr cz18_badge.GTL
+mv cz18_badge-F.Mask.gbr cz18_badge.GTS
+mv cz18_badge-F.SilkS.gbr cz18_badge.GTO
+mv cz18_badge-Edge.Cuts.gbr cz18_badge.GKO
+mv cz18_badge.drl cz18_badge.XLN
+
+
