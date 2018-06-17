8b19e982b4ade4d9c52c8939f7f1b93ceee83892
Khaled Nassar <kmhnassar@gmail.com>
2018-06-17 20:23:07 +0200

    Boilerplate for button debouncing and processing. Move the previous version into firmware.ino.old

--- firmware/firmware/firmware.ino
+++ firmware/firmware/firmware.ino
@@ -1,14 +1,3 @@
-#include <Arduino.h>
-#include <FastLED.h>
-#include <volume2.h>
-#include <IRremoteInt.h>
-#include <IRremote.h>
-#include <RCSwitch.h>
-#define LED_STRIP_PIN 4
-#define LED_STRIP_SIZE 10
-#define IR_RECV_PIN 7
-#define RC_RECV_INT 0 // Digital pin 2 == interrupt 0
-
 
 /**
  * === PIN LAYOUT ===
@@ -34,7 +23,118 @@
  * A5 - Play
  * 
  */
+ 
+#include <Arduino.h>
+#include <Bounce2.h>
 
+#define DEBUG
+#ifdef DEBUG
+#define DEBUGSERIAL(x) Serial.print(x);
+#else
+#define DEBUGSERIAL(x) { };
+#endif
+
+#define DEBUGSERIALN(x) DEBUGSERIAL(x); DEBUGSERIAL("\n");
+
+const int SLOT_COUNT = 4;
+const int MODULE_COUNT = 3;
+const int RF_MODULE = 0,
+          IR_MODULE = 1,
+          TUNE_MODULE = 2;
+
+const int BUTTON_COUNT = 4;
+const int DEBOUNCE_INTERVAL_MS = 5;
+const int MODE_BUTTON = 0,
+          SLOT_BUTTON = 1,
+          LEARN_BUTTON = 2,
+          PLAY_BUTTON = 3;
+const int BUTTON_PINS[BUTTON_COUNT] = { A2, A3, A4, A5 };
+const int BUTTON_INPUT_MODE = INPUT_PULLUP;
+const boolean BUTTON_OPEN_HIGH = true;
+const int BUTTON_UNDEFINED = -1;
+Bounce BUTTONS[BUTTON_COUNT];
+
+int module = RF_MODULE;
+int slot = 0;
+
+void setup_buttons();
+int read_pressed_button();
+void increment_switchable(int *variable, int max_count);
+
+void setup() {
+  Serial.begin(115200);
+  setup_buttons();
+}
+
+void loop() {
+  int read_button = read_pressed_button();
+  if (read_button != BUTTON_UNDEFINED) {
+    DEBUGSERIAL("Read button: ");
+    DEBUGSERIALN(read_button);
+    
+    switch(read_button) {
+      case MODE_BUTTON:
+        increment_switchable(&module, MODULE_COUNT);
+        slot = 0;
+        DEBUGSERIAL("Switched to module #");
+        DEBUGSERIALN(module);
+        break;
+      case SLOT_BUTTON:
+        if (module == RF_MODULE || module == IR_MODULE) {
+          increment_switchable(&slot, SLOT_COUNT);
+          DEBUGSERIAL("Switched to slot #");
+          DEBUGSERIAL(slot);
+          DEBUGSERIAL(" within module #");
+          DEBUGSERIALN(module);
+        }
+        break;
+      case LEARN_BUTTON:
+        // TODO: Learn according to selected module
+        break;
+      case PLAY_BUTTON:
+        // TODO: Play according to the selected module
+        break;
+      default:
+        // Do nothing if unknown
+        break;
+    }
+  }
+}
+
+void setup_buttons() {
+  for (int i = 0; i < BUTTON_COUNT; ++i) {
+    BUTTONS[i].interval(DEBOUNCE_INTERVAL_MS);
+    BUTTONS[i].attach(BUTTON_PINS[i], BUTTON_INPUT_MODE);
+  }
+}
+
+int read_pressed_button() {
+  int output = BUTTON_UNDEFINED;
+  for (int i = 0; i < BUTTON_COUNT; ++i) {
+    // Is the button pressed?
+    if (BUTTONS[i].update()) {
+      // The button is pressed if the value of read is the false when it is open high or true when it is open low
+      if (BUTTON_OPEN_HIGH != BUTTONS[i].read()) {
+        
+        if (output != BUTTON_UNDEFINED) {
+          // Already read a button, so we have two buttons pressed at the same time
+          // for now, this is undefined behaviour.
+          return BUTTON_UNDEFINED;
+        }
+        
+        output = i;
+      }
+    }
+  }
+  
+  return output;
+}
+
+void increment_switchable(int *variable, int max_count) {
+  (*variable) = ((*variable) + 1) % max_count;
+}
+
+/*
 // Define the array of leds
 CRGB leds[LED_STRIP_SIZE];
 Volume vol;
@@ -119,3 +219,4 @@ void loop() {
 }
 
 void fadeall() { for(int i = 0; i < LED_STRIP_SIZE; i++) { leds[i].nscale8(250); } }
+*/
